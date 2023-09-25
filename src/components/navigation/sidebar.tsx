99e9287cbe1e3311d375c43c16dfd91c610604f9
Dre Johnson <drejohnson212@gmail.com>
2023-09-24 23:23:19 -0400

    update

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -30,7 +30,7 @@ export default async function NavigationSidebar() {
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
-        {rooms.map((room) => (
+        {rooms.map((room: { id: string; name: string }) => (
           <div key={room.id} className="mb-4">
             <NavigationItem id={room.id} name={room.name} />
           </div>
